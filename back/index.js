cf9542c929e129d81b3d9500d0b9aaabb1f1e727
엄희성 <ehs0525@naver.com>
2023-06-06 23:55:52 +0900

    Add: Socket.IO 추가

--- back/index.js
+++ back/index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3003;
-
 const http = require("http");
 const cors = require("cors");
-
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const port = process.env.PORT || 3003;
 
 app.use(cors());
 
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Alla Till Mig!");
+});
+
+io.on("connection", (socket) => {
+  console.log(`a user connected: ${socket.id}`);
+  socket.on("disconnect", () => {
+    console.log(`a user disconnected: ${socket.id}`);
+  });
 });
 
 server.listen(port, () => {
