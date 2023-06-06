3af261da0fe7fe86b163b794ac52c823dce2ebc8
엄희성 <ehs0525@naver.com>
2023-06-07 01:52:36 +0900

    Add: react-router-dom으로 라우팅 설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import MapPage from "./pages/MapPage";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/map" element={<MapPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
