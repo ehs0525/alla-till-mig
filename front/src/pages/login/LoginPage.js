6cb834861ba69bed16537da6e4cfe55ec84f46ea
엄희성 <ehs0525@naver.com>
2023-06-08 01:45:44 +0900

    Add: Geolocation API로 현재 위치 가져오기

--- front/src/pages/login/LoginPage.js
+++ front/src/pages/login/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "./LoginPage.css";
@@ -10,10 +10,27 @@ const isUsernameValid = (username) => {
   return username.length > 0 && username.length < 10 && !username.includes(" ");
 };
 
+const success = (position) => {
+  console.log(position);
+};
+const error = (error) => {
+  console.log("Sorry, no position available.");
+  console.log(error);
+};
+const options = {
+  enableHighAccuracy: true,
+  maximumAge: 0,
+  timeout: 5000,
+};
+
 const LoginPage = () => {
   const [username, setUsername] = useState("");
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
 
   const onLogin = useCallback(() => {
     navigate("/map");
