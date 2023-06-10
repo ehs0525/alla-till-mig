376f50c2eb93bdfbdf78e833c0f5c20b87e8f169
엄희성 <ehs0525@naver.com>
2023-06-10 22:46:57 +0900

    Add: 마커에 유저이름 달기

--- front/src/pages/map/Marker.js
+++ front/src/pages/map/Marker.js
@@ -5,6 +5,7 @@ const Marker = ({ socketID, username, coords, isMe }) => {
   return (
     <div className="marker_container">
       <img src={MapMarker} alt={username} className="marker_img" />
+      <p className="marker_text">{isMe ? "Me" : username}</p>
     </div>
   );
 };
