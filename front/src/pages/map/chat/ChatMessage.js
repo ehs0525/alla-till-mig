72ca80cbc7f363b9e0f028218cb0785cbe789499
엄희성 <ehs0525@naver.com>
2023-06-14 04:43:44 +0900

    Add: Popup Chat Window에 채팅 부분 추가

---
+++ front/src/pages/map/chat/ChatMessage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatMessage = ({ socketID }) => {
+  return <div className="chat_message_container"></div>;
+};
+
+export default ChatMessage;
--- front/src/pages/map/chat/PopupChatWindow.js
+++ front/src/pages/map/chat/PopupChatWindow.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import NavBar from "./NavBar";
+import ChatMessage from "./ChatMessage";
 
 const PopupChatWindow = ({ username, socketID }) => {
   return (
     <div className="popup_chat_window_container">
       <NavBar username={username} socketID={socketID} />
+      <ChatMessage />
     </div>
   );
 };
