34ca2cdcb13ce98da677835ad83e99a4e7c023cb
엄희성 <ehs0525@naver.com>
2023-06-24 21:36:52 +0900

    Add: Video Button Group

--- front/src/pages/map/video/VideoButtonGroup.js
+++ front/src/pages/map/video/VideoButtonGroup.js
@@ -6,9 +6,8 @@ const VideoButtonGroup = () => {
   return (
     <div className="video_btns_container">
       <img src={CallSlash} alt="card-slash" className="video_btn" />
-
-      <button className="video_btn">B</button>
-      <button className="video_btn">C</button>
+      <img src={CallSlash} alt="card-slash" className="video_btn" />
+      <img src={CallSlash} alt="card-slash" className="video_btn" />
     </div>
   );
 };
